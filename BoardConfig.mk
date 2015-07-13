bbd848fb352977304155313dd336e081eea37f06
Kryten2k35 <Kryten2k35@gmail.com>
2015-07-13 12:00:52 +0100

    Removed TWRP properties

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,14 +24,13 @@ TARGET_CPU_VARIANT := krait
 TARGET_NO_BOOTLOADER := true
 
 # Inline kernel building
-TARGET_KERNEL_CONFIG := shamu_defconfig
-TARGET_KERNEL_SOURCE := kernel/moto/shamu
-TARGET_KERNEL_CUSTOM_TOOLCHAIN :=
-
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+TARGET_KERNEL_CONFIG := shamu_defconfig
+TARGET_KERNEL_SOURCE := kernel/moto/shamu
 
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=shamu msm_rtb.filter=0x37 ehci-hcd.park=3 utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags utags.backup=/dev/block/platform/msm_sdcc.1/by-name/utagsBackup coherent_pool=8M
 
@@ -181,14 +180,5 @@ EXTENDED_FONT_FOOTPRINT := true
 
 # Enable workaround for slow rom flash
 BOARD_SUPPRESS_SECURE_ERASE := true
-
-# TWRP
-TW_THEME := portrait_hdpi
-TW_INCLUDE_L_CRYPTO := true
-BOARD_HAS_NO_REAL_SDCARD := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
-TW_SCREEN_BLANK_ON_BOOT := true
-DEVICE_RESOLUTION := 1440x2560
 
 -include vendor/moto/shamu/BoardConfigVendor.mk
